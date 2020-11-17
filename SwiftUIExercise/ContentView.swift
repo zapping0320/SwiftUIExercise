ed85dea959faa67f63c002649cd062f159accb2d
john donghyun kim <zapping0320@gmail.com>
2020-11-17 11:38:00 +0900

    Initial Commit

---
+++ SwiftUIExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExercise
+//
+//  Created by DD Dev on 2020/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIExercise/SwiftUIExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExerciseApp.swift
+//  SwiftUIExercise
+//
+//  Created by DD Dev on 2020/11/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
