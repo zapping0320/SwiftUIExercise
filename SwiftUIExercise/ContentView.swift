ba7c5e646916cf5ea272885933acae02d09e16f8
john donghyun kim <zapping0320@gmail.com>
2020-11-17 13:15:05 +0900

    NavigationView / Form 으로 포함함

--- SwiftUIExercise/ContentView.swift
+++ SwiftUIExercise/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name = ""
+    @State var age = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    TextField("이름을 입력해주세요", text: $name)
+                        .keyboardType(.default) //키보드를 기본 키보드로 보여줍니다.
+                }
+                Section{
+                    TextField("나이를 입력해주세요", text: $age)
+                        .keyboardType(.numberPad) // number형식의 키보드를 보여줍니다.
+                    
+                }
+                Section{
+                    Text("\(name)님의 나이는 \(age)살입니다. ")
+                }
+            }.navigationBarTitle("회원가입")
+        }
     }
 }
 
